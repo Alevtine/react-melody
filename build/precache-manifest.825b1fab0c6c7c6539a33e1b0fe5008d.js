a520cbef12feac3db9a624167ec0ca8582265873
Alevtine <mirada.insolente@gmail.com>
2018-10-31 12:25:59 +0300

    build folder added

---
+++ build/precache-manifest.825b1fab0c6c7c6539a33e1b0fe5008d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c1b8ddf13a2827ff9b78d1e309a8d27",
+    "url": "/static/media/FiraSans-Light.8c1b8ddf.woff"
+  },
+  {
+    "revision": "9eb020c16d9e2e622e75",
+    "url": "/static/css/main.40a74701.chunk.css"
+  },
+  {
+    "revision": "d42a4f97535d74df7101",
+    "url": "/static/js/1.d42a4f97.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af2bba1dfc9097345123e830a3bbd437",
+    "url": "/static/media/vinyl.af2bba1d.png"
+  },
+  {
+    "revision": "f853a67e241f4d9bc58c0ea22bca8fbb",
+    "url": "/static/media/right-arrow.f853a67e.svg"
+  },
+  {
+    "revision": "7b66b7538b756650b9c6ef71dbca1cd6",
+    "url": "/static/media/icon-cross.7b66b753.svg"
+  },
+  {
+    "revision": "0abf96f6d741f230c2e93da66b90a8e6",
+    "url": "/static/media/FiraSans-Light.0abf96f6.woff2"
+  },
+  {
+    "revision": "9eb020c16d9e2e622e75",
+    "url": "/static/js/main.9eb020c1.chunk.js"
+  },
+  {
+    "revision": "43231cec248f0f2732804327909f0cdf",
+    "url": "/static/media/FiraSans-LightItalic.43231cec.woff2"
+  },
+  {
+    "revision": "bea02e99e18f9259a1cea894b02aa336",
+    "url": "/static/media/FiraSans-LightItalic.bea02e99.woff"
+  },
+  {
+    "revision": "f2d11874060a4b0102b521b9868fc603",
+    "url": "/static/media/FiraSans-Medium.f2d11874.woff2"
+  },
+  {
+    "revision": "6d087365d398ef48b76b937dfa780e7c",
+    "url": "/static/media/FiraSans-Medium.6d087365.woff"
+  },
+  {
+    "revision": "519c8d796897ae05d9867aadae3c2a5a",
+    "url": "/static/media/FiraSans-MediumItalic.519c8d79.woff2"
+  },
+  {
+    "revision": "aca49b3ea07ca569e89dce3128af207a",
+    "url": "/static/media/FiraSans-MediumItalic.aca49b3e.woff"
+  },
+  {
+    "revision": "34e61ed13ab58659cfc3c7e4cb02dba9",
+    "url": "/index.html"
+  }
+];
